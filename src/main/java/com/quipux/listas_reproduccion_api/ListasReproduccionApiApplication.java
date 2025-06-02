1e023fcc25e9d7ef11b6f601e8d6bb4ac304c89c
MalloriLopez <mallorilopez1@gmail.com>
2025-06-02 09:29:49 -0500

    se inicia el desarrollo de la prueba

---
+++ src/main/java/com/quipux/listas_reproduccion_api/ListasReproduccionApiApplication.java
@@ -0,0 +1,13 @@
+package com.quipux.listas_reproduccion_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListasReproduccionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListasReproduccionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quipux/listas_reproduccion_api/ListasReproduccionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quipux.listas_reproduccion_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListasReproduccionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
